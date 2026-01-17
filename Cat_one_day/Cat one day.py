f0cec80dd80770fdb7cf5ccdbd8384baaa9a4fc4
Power <58568260+zeroootwo@users.noreply.github.com>
2026-01-17 22:04:27 +0200

    Create a learning dir

---
+++ Cat_one_day/Cat one day.py
@@ -0,0 +1,25 @@
+import os
+import time
+
+time.sleep(1)
+while True:
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(r"""
+      /\_/\
+     ( o.o )
+      > ^ <
+     /     \
+    |       |\__
+     \_____/  `*)
+    """)
+    time.sleep(3)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(r"""
+      /\_/\
+     ( -.- )
+      > ^ <
+     /     \
+    |       |\__
+     \_____/  `*)
+    """)
+    time.sleep(0.2)
