bc50ba52e390c9e4fc548a772b5676454d6453a8
Anastasiia Velikaya <a.v.zaychenko@gmail.com>
2026-01-17 23:40:00 +0200

    Добавлено главное меню игры, реализована функция выхода из игры, добавлены функции, которые будут описаны позже

--- Cat_one_day/Cat one day.py
+++ Cat_one_day/Cat one day.py
@@ -1,25 +1,61 @@
 import os
+import sys
 import time
 
-time.sleep(1)
-while True:
+def cat_anim():
+    time.sleep(1)
+    while True:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print(r"""
+          /\_/\
+         ( o.o )
+          > ^ <
+         /     \
+        |       |\__
+         \_____/  `*)
+        """)
+        time.sleep(3)
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print(r"""
+          /\_/\
+         ( -.- )
+          > ^ <
+         /     \
+        |       |\__
+         \_____/  `*)
+        """)
+        time.sleep(0.2)
+
+def start_game():
     os.system('cls' if os.name == 'nt' else 'clear')
-    print(r"""
-      /\_/\
-     ( o.o )
-      > ^ <
-     /     \
-    |       |\__
-     \_____/  `*)
-    """)
-    time.sleep(3)
+    print("Under Construction")
+
+def list_score():
     os.system('cls' if os.name == 'nt' else 'clear')
-    print(r"""
-      /\_/\
-     ( -.- )
-      > ^ <
-     /     \
-    |       |\__
-     \_____/  `*)
-    """)
-    time.sleep(0.2)
+    print("Under Construction")
+
+def exit_game():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    ans_1=input("Вы уверены, что хотите выйти из игры?(Y/N): ")
+    match ans_1:
+        case "Y" | "y":
+            print("До встречи!")
+            sys.exit()
+        case "N" | "n":
+            return
+
+def main():
+    while True:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("Кошачий переполох")
+        print("""\t1. Играть!
+        \t2. Таблица рекордов.
+        \t3. Выход :(""")
+        ans=int(input("Выберите пункт меню: "))
+        match ans:
+            case 1:
+                start_game()
+            case 2:
+                list_score()
+            case 3:
+                exit_game()
